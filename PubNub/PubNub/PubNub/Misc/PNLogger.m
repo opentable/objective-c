3e514084052d6c2983d6d9ee41e402a96eaa2206
Alex Jarvis <alex@panaxiom.co.uk>
2015-03-19 12:01:18 +0000

    Fix for framework bundle path

--- PubNub/PubNub/PubNub/Misc/PNLogger.m
+++ PubNub/PubNub/PubNub/Misc/PNLogger.m
@@ -1348,7 +1348,7 @@ typedef NS_OPTIONS(NSUInteger, PNLoggerConfiguration) {
     NSString *symbolsPath = [[NSBundle mainBundle] pathForResource:@"PNLoggerSymbols" ofType:@"plist"];
     if (!symbolsPath) {
         
-        NSBundle *frameworkResources = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"PubNub"
+        NSBundle *frameworkResources = [NSBundle bundleWithPath:[[NSBundle bundleForClass:[self class]] pathForResource:@"PubNub"
                                                                                                 ofType:@"bundle"]];
         symbolsPath = [frameworkResources pathForResource:@"PNLoggerSymbols" ofType:@"plist"];;
     }
