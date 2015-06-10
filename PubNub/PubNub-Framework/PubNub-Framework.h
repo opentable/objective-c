3a5dba669e788a58642515473fa3a382ec218f16
Kra Larivain <olarivain@gmail.com>
2015-06-09 18:07:45 -0700

    added carthage target

---
+++ PubNub/PubNub-Framework/PubNub-Framework.h
@@ -0,0 +1,19 @@
+//
+//  PubNub-Framework.h
+//  PubNub-Framework
+//
+//  Created by Olivier Larivain on 6/9/15.
+//  Copyright © 2015 PubNub, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PubNub-Framework.
+FOUNDATION_EXPORT double PubNub_FrameworkVersionNumber;
+
+//! Project version string for PubNub-Framework.
+FOUNDATION_EXPORT const unsigned char PubNub_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PubNub_Framework/PublicHeader.h>
+
+
