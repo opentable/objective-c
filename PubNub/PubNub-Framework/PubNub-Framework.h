e9ab829ea40eac2cf3e9848156520edfbdf6acd9
Kra Larivain <olarivain@gmail.com>
2015-06-09 19:05:29 -0700

    fixing umbrella header

--- PubNub/PubNub-Framework/PubNub-Framework.h
+++ PubNub/PubNub-Framework/PubNub-Framework.h
@@ -15,5 +15,43 @@ FOUNDATION_EXPORT double PubNub_FrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char PubNub_FrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PubNub_Framework/PublicHeader.h>
-
-
+#import <PubNub/PNAccessRightsInformation.h>
+#import <PubNub/PNAccessRightsCollection.h>
+#import <PubNub/PNAccessRightOptions.h>
+#import <PubNub/PNObservationCenter.h>
+#import <PubNub/PNChannelPresence.h>
+#import <PubNub/PNConfiguration.h>
+#import <PubNub/PNNotifications.h>
+#import <PubNub/PNDelegate.h>
+#import <PubNub/PNWhereNow.h>
+#import <PubNub/PNHereNow.h>
+#import <PubNub/PNNetworkHelper.h>
+#import <PubNub/NSString+PNAddition.h>
+#import <PubNub/PNCryptoHelper.h>
+#import <PubNub/PNErrorCodes.h>
+#import <PubNub/PNStructures.h>
+#import <PubNub/PNChannelGroupNamespace.h>
+#import <PubNub/PNChannelGroupChange.h>
+#import <PubNub/PNChannelProtocol.h>
+#import <PubNub/PNPresenceEvent.h>
+#import <PubNub/PNChannelGroup.h>
+#import <PubNub/PNMessage.h>
+#import <PubNub/PNChannel.h>
+#import <PubNub/PNClient.h>
+#import <PubNub/PNLogger.h>
+#import <PubNub/PNMacro.h>
+#import <PubNub/PNError.h>
+#import <PubNub/PNDate.h>
+#import <PubNub/PubNub+ChannelRegistry.h>
+#import <PubNub/PubNub+PresenceEvents.h>
+#import <PubNub/PubNub+Subscription.h>
+#import <PubNub/PubNub+Messaging.h>
+#import <PubNub/PubNub+Presence.h>
+#import <PubNub/PubNub+History.h>
+#import <PubNub/PubNub+Cipher.h>
+#import <PubNub/PubNub+State.h>
+#import <PubNub/PubNub+Time.h>
+#import <PubNub/PubNub+APNS.h>
+#import <PubNub/PubNub+PAM.h>
+#import <PubNub/PubNub.h>
+#import <PubNub/PNImports.h>
